3273795d083bb91ccc013e2d0f8370435d9703ec
loganfrench <32106564+loganfrench@users.noreply.github.com>
2017-09-19 22:28:50 +0300

    Add files via upload

---
+++ test-1/job-4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+double transferFahr(float cels) {
+	return 1.8*cels+32.0;
+}
+
+int main(int argc, char** argv) 
+{
+	setlocale(LC_ALL,"Russian");
+
+	float temp;
+	cout << "Введите температуру градусов Цельсия" << endl;
+	cin >> temp;
+	cout << temp << " градусов Цельсия равняется " << transferFahr(temp) << " градусов Фаренгейта" << endl;
+
+	return 0;
+}
