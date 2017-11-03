98c21717b7e539cf1d8541e791c08349d0cd37a3
Timur Azizov <32106564+loganfrench@users.noreply.github.com>
2017-11-03 19:08:50 +0300

    Add files via upload

---
+++ labs/job-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+ 
+ 
+using namespace std;
+ 
+int main()
+{
+    double xn,xk,dx, a,b,c;
+    cin >> xn >> xk >> dx >> a >> b >> c ;
+    if(int(a) | int(b) & (int(a) | int(c)) != 0) {
+        for(double x = xn; x <= xk; x += dx) {
+            double f;
+            if(x < 0 && b != 0) f = a*pow(x,2)+b;
+            else if(x > 0 && b == 0) f = x-a/x-c;
+            else f = x/c;
+            cout << "x = " << x << "\tf = " << f << endl;
+        }  
+    }
+    else {
+        for(double x = xn; x <= xk; x += dx) {
+            int f;
+            if(x < 0 && b != 0) f = a*pow(x,2)+b;
+            else if(x > 0 && b == 0) f = x-a/x-c;
+            else f = x/c;
+            cout << "x = " << x << "\tf = " << f << endl;
+        }
+    }
+    return 0;
+}
