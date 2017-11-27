1347bdb772201aef4fca1ec2079cce0133c651b5
Timur Azizov <32106564+loganfrench@users.noreply.github.com>
2017-11-27 22:07:50 +0300

    Add files via upload

---
+++ labs/job-4/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+struct STUDENT { 
+	string family; 
+	int group; 
+	float rat[5]; 
+};
+
+int main() 
+{   
+	setlocale(LC_ALL, "Russian");
+	const int SIZE = 10;
+	STUDENT stud[SIZE];
+	int goodStud = 0;
+	
+	for (int i = 0; i < SIZE; i++){
+		cout << "‚ведите фамилию студента: ";
+		cin >> stud[i].family;
+		cout << "‚ведите номер группы: ";
+		cin >> stud[i].group;
+		cout << "‚ведите 5 оценок: " << endl;
+		for (int j = 0; j < 5; j++) cin >> stud[i].rat[j];
+		cout << endl;
+	}
+	
+	double sum = 0;
+	for (int i = 0; i < SIZE; i++) {
+		
+		for (int j = 0; j < 5; j++) sum += stud[i].rat[j];
+		
+		double score = sum / 5;
+		
+		if(score > 4.0) {
+			cout << "”амилия студента: " << stud[i].family << endl;
+			cout << "ѓруппа: " << stud[i].group << endl;
+			cout << "Балл: " << score << endl;
+			goodStud++;
+			cout << endl;
+		}
+		sum = 0;
+	}
+	if(!goodStud) cout << "Нет студентов со средним баллом больше 4.0" << endl;
+	
+	return 0;
+}
